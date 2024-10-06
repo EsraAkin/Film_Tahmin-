bedf6bbfe49d87d28027326fa13e41434d85b4e5
Esra Akineden <e.akineden@gmail.com>
2024-10-06 20:41:01 +0200

    Film Tahmin v1

--- src/main/java/Film_Secimi.java
+++ src/main/java/Film_Secimi.java
@@ -12,16 +12,19 @@ public class Film_Secimi {
         System.out.println("0 ile " + (filmler.size() - 1) + " arasında bir numara seçiniz. ");
         int kullaniciIndex = input.nextInt();
         String secilenfilm = filmler.get(kullaniciIndex);
-        System.out.println(secilenfilm);
+        //  System.out.println(secilenfilm);
         System.out.println("Seçtiğiniz Filmin harf sayısı: " + secilenfilm.length());
 
         int tahminHakki = 2 * secilenfilm.length();        // Tahmin hakkı, harf sayısının 2 katı
         System.out.println("Filmi doğru tahmin etmek için " + tahminHakki + " hakkınız vardır.");
 
         String[] bos = new String[secilenfilm.length()];   // Tahmin edilen harfler için boş bir dizi
-        Arrays.fill(bos, "_");                          // İlk başta boşluğu (_) göster.
+        for (int i = 0; i < bos.length; i++) {
+            bos[i] = "_";  // İlk başta boşluğu (_) göster.
+        }
 
-        char[] harfler =secilenfilm.toCharArray();        // Filmin harflerini tutan dizi
+
+        char[] harfler = secilenfilm.toCharArray();        // Filmin harflerini tutan dizi
 
         int kalanHakSayisi = tahminHakki;                 // Kalan hakları kontrol etmek için
         int dogruTahminSayisi = 0;
@@ -32,16 +35,16 @@ public class Film_Secimi {
             // Tahmin edilen filmin mevcut durumunu güncelle.
             String tahminDurumu = "";
             for (String w : bos) {
-                tahminDurumu += w + " ";  // Her harfi boşluk ile birleştiriyoruz
+                tahminDurumu += w + " ";   //Harf tahmini doğru ise _ karakterini harf ile değiştir.
             }
 
             System.out.println("Tahmin edilen film: " + tahminDurumu.trim());  // O anki tahmin durumu
             System.out.println("Bir harf tahmin ediniz:");
             char kullaniciHarf = input.next().charAt(0);  // Kullanıcıdan harf alıyoruz
 
-            boolean harfVarmi = false;  // Harf bulunup bulunmadığını kontrol etmek için
+            boolean harfVarmi = false;  // Harf bulunup bulunmadığını kontrol etmek için başlangıç
             for (int i = 0; i < harfler.length; i++) {
-                if (harfler[i] == kullaniciHarf) {   // Harfi kontrol ediyoruz
+                if (harfler[i] == kullaniciHarf) {   // Harfi kontrol et
                     bos[i] = String.valueOf(kullaniciHarf);  // Eğer doğru tahminse, diziye ekliyoruz
                     harfVarmi = true;
                 }
@@ -57,9 +60,8 @@ public class Film_Secimi {
 
             kalanHakSayisi--;
 
-                System.out.println("Doğru Tahmin Sayısı: "+dogruTahminSayisi);
-                System.out.println("Yanlış Tahmin Sayısı: "+yanlisTahminSayisi);
-
+            System.out.println("Doğru Tahmin Sayısı: " + dogruTahminSayisi);
+            System.out.println("Yanlış Tahmin Sayısı: " + yanlisTahminSayisi);
 
 
             // Oyuncu filmi doğru tahmin etti mi?
@@ -70,7 +72,7 @@ public class Film_Secimi {
 
             if (tahminStringi.equals(secilenfilm)) {
                 kazandiMi = true;
-                break;  // Tüm harfleri doğru bildiyse döngüyü bitiriyoruz
+                break;  // Tüm harfleri doğru bildiyse döngüyü bitir
             }
 
             System.out.println("Kalan hak sayısı: " + kalanHakSayisi);
